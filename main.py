a1c758a03aaf1aae3221416354017e4625467481
Daevik Jain <daevikjain@gmail.com>
2025-06-10 22:43:24 -0400

    Optimization

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 from matplotlib.collections import LineCollection
 import random
+from collections import defaultdict
 
 # Simulation parameters
 radius = 5.0
@@ -14,7 +15,8 @@ dt = 0.05
 revolutions = 3
 frame_number = 1000
 rotation_speed = 2 * revolutions * np.pi / frame_number
-trail_length = 25 
+trail_length = 25
+cell_size = 2 * ball_radius + 0.1
 
 # Figure setup
 fig, ax = plt.subplots()
@@ -29,14 +31,9 @@ for spine in ax.spines.values():
     spine.set_visible(False)
 
 # Objects
-ball_positions = []
-ball_velocities = []
-ball_patches = []
-ball_colors = []
+ball_positions, ball_velocities, ball_colors = [], [], []
+ball_patches, ball_trails, ball_trail_collections = [], [], []
 ball_escaped_flags = []
-ball_collision_cooldowns = []
-ball_trails = []
-ball_trail_collections = []
 
 circle_edge, = ax.plot([], [], 'b-')
 hole_patch, = ax.plot([], [], 'k', lw=4)
@@ -66,7 +63,6 @@ def add_ball(pos, vel):
     ball_colors.append(color)
     ball_patches.append(ax.plot([], [], 'o', ms=8, color=color)[0])
     ball_escaped_flags.append(False)
-    ball_collision_cooldowns.append({})
     ball_trails.append([])
 
     trail_collection = LineCollection([], linewidths=[], colors=[], alpha=1.0)
@@ -82,54 +78,41 @@ def spawn_two_balls():
         add_ball([dx, random_y], random_vel)
 
 def handle_ball_collisions():
-    n = len(ball_positions)
-    if n < 2:
-        return
-    for i in range(n):
-        cooldown_dict = ball_collision_cooldowns[i]
-        keys_to_remove = [k for k, v in cooldown_dict.items() if v <= 0]
-        for k in keys_to_remove:
-            del cooldown_dict[k]
-        for k in cooldown_dict:
-            cooldown_dict[k] -= 1
+    grid = defaultdict(list)
+    for idx, pos in enumerate(ball_positions):
+        cell = tuple((pos // cell_size).astype(int))
+        grid[cell].append(idx)
 
-    min_separation = 2 * ball_radius + 0.03
-    cooldown_frames = 10
-
-    for i in range(n):
-        for j in range(i + 1, n):
-            if j in ball_collision_cooldowns[i] or i in ball_collision_cooldowns[j]:
+    checked_pairs = set()
+    for cell, indices in grid.items():
+        neighbors = [(i, j) for dx in [-1, 0, 1] for dy in [-1, 0, 1]
+                     for i in grid.get((cell[0] + dx, cell[1] + dy), [])
+                     for j in indices if i < j]
+        for i, j in neighbors:
+            if (i, j) in checked_pairs:
                 continue
+            checked_pairs.add((i, j))
+
             pos_i, pos_j = ball_positions[i], ball_positions[j]
             diff = pos_j - pos_i
-            distance = np.linalg.norm(diff)
-            if distance < min_separation and distance > 1e-10:
-                normal = diff / distance
-                overlap = min_separation - distance
-                separation = overlap / 2 + 0.02
-                ball_positions[i] -= normal * separation
-                ball_positions[j] += normal * separation
+            dist = np.linalg.norm(diff)
+            if dist < 2 * ball_radius and dist > 1e-10:
+                normal = diff / dist
+                overlap = 2 * ball_radius - dist
+                correction = normal * overlap / 2
+                ball_positions[i] -= correction
+                ball_positions[j] += correction
 
                 vi, vj = ball_velocities[i], ball_velocities[j]
                 vi_n, vj_n = np.dot(vi, normal), np.dot(vj, normal)
-                rel_velocity = vi_n - vj_n
-                if rel_velocity < 0:
+                if vi_n - vj_n < 0:
+                    restitution = 0.85
                     vi_t = vi - vi_n * normal
                     vj_t = vj - vj_n * normal
-                    restitution = 0.85
-                    new_vi_n = vj_n * restitution
-                    new_vj_n = vi_n * restitution
-                    ball_velocities[i] = new_vi_n * normal + vi_t
-                    ball_velocities[j] = new_vj_n * normal + vj_t
+                    ball_velocities[i] = (vj_n * restitution) * normal + vi_t
+                    ball_velocities[j] = (vi_n * restitution) * normal + vj_t
 
-                    perturb = 0.05 * np.random.randn(2)
-                    ball_velocities[i] += perturb
-                    ball_velocities[j] -= perturb
-
-                    ball_collision_cooldowns[i][j] = cooldown_frames
-                    ball_collision_cooldowns[j][i] = cooldown_frames
-
-# Initialize first ball
+# Initialize
 add_ball([0.0, 0.0], [1.5, 2.0])
 
 def update(frame):
@@ -143,59 +126,52 @@ def update(frame):
         vel[1] -= gravity
         pos += vel * dt
 
-        distance = np.linalg.norm(pos)
-        escaped = ball_escaped_flags[i]
-        if not escaped and distance + ball_radius >= radius:
-            edge_point = pos * ((radius - ball_radius) / distance)
+        dist = np.linalg.norm(pos)
+        if not ball_escaped_flags[i] and dist + ball_radius >= radius:
+            edge_point = pos * ((radius - ball_radius) / dist)
             if is_in_hole(edge_point, theta):
                 ball_escaped_flags[i] = True
             else:
-                normal = pos / distance
-                vel_n = np.dot(vel, normal)
-                vel -= 2 * vel_n * normal
+                normal = pos / dist
+                vel -= 2 * np.dot(vel, normal) * normal
                 pos = normal * (radius - ball_radius - 0.001)
 
         if abs(pos[0]) > limit or abs(pos[1]) > limit:
             to_remove.append(i)
 
-        ball_positions[i], ball_velocities[i] = pos, vel
+        ball_positions[i] = pos
+        ball_velocities[i] = vel
 
+        # Trail update
         ball_trails[i].append(pos.copy())
         if len(ball_trails[i]) > trail_length:
             ball_trails[i].pop(0)
-
         trail = np.array(ball_trails[i])
         if len(trail) >= 2:
             segments = [[trail[j], trail[j+1]] for j in range(len(trail)-1)]
             widths = np.linspace(3.5, 0.5, len(segments))
             alphas = np.linspace(0.1, 0.7, len(segments))
-            rgba = [(*ball_colors[i], alpha) for alpha in alphas]
+            rgba = [(*ball_colors[i], a) for a in alphas]
             ball_trail_collections[i].set_segments(segments)
             ball_trail_collections[i].set_linewidths(widths)
             ball_trail_collections[i].set_colors(rgba)
         else:
             ball_trail_collections[i].set_segments([])
 
+    # Remove dead balls
     for i in reversed(to_remove):
         ball_patches[i].remove()
         ball_trail_collections[i].remove()
         del ball_positions[i]
         del ball_velocities[i]
-        del ball_patches[i]
         del ball_colors[i]
+        del ball_patches[i]
         del ball_escaped_flags[i]
-        del ball_collision_cooldowns[i]
         del ball_trails[i]
         del ball_trail_collections[i]
-        for cooldown in ball_collision_cooldowns:
-            keys = list(cooldown.keys())
-            for key in keys:
-                if key == i:
-                    del cooldown[key]
-                elif key > i:
-                    cooldown[key - 1] = cooldown.pop(key)
         spawn_two_balls()
 
+    # Draw everything
     for i, pos in enumerate(ball_positions):
         ball_patches[i].set_data([pos[0]], [pos[1]])
 
